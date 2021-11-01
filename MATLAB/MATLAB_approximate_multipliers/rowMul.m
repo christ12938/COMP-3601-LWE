5ae9ac0446d1198a1d372b95f280c144b7745c41
Tirth <tirthparikh27@gmail.com>
2021-11-01 19:19:10 +1100

    Prints added to matlab

--- MATLAB/MATLAB_approximate_multipliers/rowMul.m
+++ MATLAB/MATLAB_approximate_multipliers/rowMul.m
@@ -10,11 +10,15 @@ B_element = 0;
 
 for i = 1:length(row_a)
 %      [result , ~,~] = fn_MitchellMul_Optimized(row_a(i),S(i),32);
-%     [result , ~,~] = fn_MitchellMul_MBM_t(row_a(i),S(i),8,6);
-    [result , ~,~] = fn_MitchellMul_REALM8x8_t(row_a(i),S(i),16,15);
+    [result , ~,~] = fn_MitchellMul_MBM_t(row_a(i),S(i),16,15);
+%     [result , ~,~] = fn_MitchellMul_REALM8x8_t(row_a(i),S(i),16,15);
 %     result = row_a(i)*S(i);
     B_element = B_element + result;
-    disp((row_a(i)*S(i)) - result);
+%     disp("A=");
+%     disp(row_a(i));
+%     disp("S=");
+%     disp(S(i));
+%     disp((row_a(i)*S(i)) - result);
 end
 %disp(B_element);
 %disp(row_a*S);
